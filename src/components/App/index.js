818a992787e4b6aed851c6b56d2ac681cd6c85d5
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-03-15 21:27:14 -0300

    created Banner component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from '../Header';
+import Banner from '../Banner';
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   )
 };
---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Hero from '../../assets/images/Hero-banner.svg'
+
+function Banner() {
+  return (
+    <Hero />
+  )
+}
+
+export default Banner;
