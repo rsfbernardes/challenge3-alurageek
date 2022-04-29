cfa63afa4f10b0fee44e9996f3f4562db599f74c
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-04-28 22:57:11 -0300

    configured url-loader

--- src/components/ProductGallery/ProductCard.js
+++ src/components/ProductGallery/ProductCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import StarWars from '../../assets/images/star-wars.png';
+
 function ProductCard() {
   return (
     <>
       <>
-        <img src="" alt="" />
+        <img src={StarWars} alt="" />
         <h4>Produto XYZ</h4>
         <p>R$60,00</p>
         <a href="#" target='_blank'>Ver Produto</a>
--- webpack.config.js
+++ webpack.config.js
@@ -28,14 +28,7 @@ module.exports = {
     },
     {
       test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
+        use: 'url-loader'
     },
   ],
   },
